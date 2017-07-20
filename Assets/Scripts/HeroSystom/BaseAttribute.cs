cf7c4172732347fd2c7cd29c328878e2f383ccc7
TaoTng <578467957@qq.com>
2017-07-20 22:11:16 +0800

    Signed-off-by: TaoTng <578467957@qq.com>

---
+++ Assets/Scripts/HeroSystom/BaseAttribute.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HeroSystom
+{
+    /// <summary>
+    /// 英雄的基础属性
+    /// </summary>
+    public class BaseAttribute
+    {
+        /// <summary>
+        /// 初始最大生命值
+        /// </summary>
+        private int _baseLife;
+
+        public int baseLife
+        {
+            get { return _baseLife; }
+            set { this._baseLife = value; }
+        }
+
+        /// <summary>
+        /// 初始最大法力值
+        /// </summary>
+        private int _baseMagic;
+
+        public int baseMagic
+        {
+            get { return _baseMagic; }
+            set { _baseMagic = value; }
+        }
+
+        /// <summary>
+        /// 物理攻击
+        /// </summary>
+        private int _physicalAttack;
+
+        public int physicalAttack
+        {
+            get { return _physicalAttack; }
+            set { _physicalAttack = value; }
+        }
+
+        /// <summary>
+        /// 法术攻击
+        /// </summary>
+        private int _magicAttack;
+
+        public int magicAttack
+        {
+            get { return _magicAttack; }
+            set { _magicAttack = value; }
+        }
+
+        /// <summary>
+        /// 法术防御
+        /// </summary>
+        private int _magicDefense;
+
+        public int magicDefense
+        {
+            get { return _magicDefense; }
+            set { _magicDefense = value; }
+        }
+
+        /// <summary>
+        /// 物理防御
+        /// </summary>
+        private int _physicalDefense;
+
+        public int physicalDefense
+        {
+            get { return _physicalDefense; }
+            set { _physicalDefense = value; }
+        }
+    }
+}
---
+++ Assets/Scripts/HeroSystom/DefenseAttribute.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace HeroSystom
+{
+    /// <summary>
+    /// 人物角色的防御属性
+    /// </summary>
+    public class DefenseAttribute
+    {
+
+        /// <summary>
+        /// 韧性
+        /// </summary>
+        private float _tenacity;
+
+        public float tenacity
+        {
+            get { return _tenacity; }
+            set { _tenacity = value; }
+        }
+
+        /// <summary>
+        /// 每5秒回血
+        /// </summary>
+        private int _recoveBlood;
+
+        public int recoveBlood
+        {
+            get { return _recoveBlood; }
+            set { _recoveBlood = value; }
+        }
+
+        /// <summary>
+        /// 每5秒回蓝
+        /// </summary>
+        private int _recoveMagic;
+
+        public int recoveMagic
+        {
+            get { return _recoveMagic; }
+            set { _recoveMagic = value; }
+        }
+    }
+}
